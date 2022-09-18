fa6990403ba59938a4d4434c0ac7686b85e147b1
riulwoo <geun10121@naver.com>
2022-09-18 21:30:38 +0900

    220918 init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>온라인 육목 공사중</div>
+    </div>
+  );
+}
+
+export default App;
